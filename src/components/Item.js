9ae396ed69ee3b0f03c30038ffc4cceaaca3323a
unknown <aatosm@hotmail.com>
2017-12-17 16:25:38 +0200

    Added prettier outlook for timestamp on list-item

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,11 +4,20 @@ import { ListGroupItem } from 'react-bootstrap';
 class Item extends Component {
 
   render() {
+
+    let a = this.props.item.dateTime.split("T");
+    let date = a[0].split("-")[2];
+    let month = a[0].split("-")[1];
+    let year = a[0].split("-")[0];
+    let hours = a[1].split(":")[0];
+    let minutes = a[1].split(":")[1];
+    let timeStamp = date+"."+month+"."+year+" ("+hours+":"+minutes+")";
+
     return (
       <ListGroupItem header={this.props.item.species} >
         Description: {this.props.item.description}<br />
         Count: <b>{this.props.item.count}</b><br />
-        Time: {this.props.item.dateTime}
+        <i>Added on {timeStamp}</i>
       </ListGroupItem>
     );
   }
